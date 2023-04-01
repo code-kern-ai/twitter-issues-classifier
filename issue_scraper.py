cc489d77b014a3a7b1df16811ea39a359770c394
Johannes Hötter <johannes.hoetter@kern.ai>
2023-04-01 13:06:26 +0200

    update scraper to get all current issues, but only reduced attribute set

--- issue_scraper.py
+++ issue_scraper.py
@@ -8,12 +8,37 @@ repo = "the-algorithm"
 url = f"https://api.github.com/repos/{owner}/{repo}/issues"
 headers = {"Authorization": f"Bearer {PAT}"}
 
-response = requests.get(url, headers=headers)
+all_issues = []
 
-if response.status_code == 200:
-    issues = response.json()
-    with open("twitter-issues.json", "w") as f:
-        json.dump(issues, f)
-    print("Issues saved to issues.json")
-else:
-    print(f"Failed to retrieve issues (status code {response.status_code}): {response.text}")
+while url:
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        issues = response.json()
+        all_issues.extend(issues)
+        if "next" in response.links:
+            url = response.links["next"]["url"]
+        else:
+            url = None
+    else:
+        print(f"Failed to retrieve issues (status code {response.status_code}): {response.text}")
+        break
+
+issues_reduced = []
+for issue in all_issues:
+    issue_reduced = {
+        "title": issue["title"],
+        "body": issue["body"],
+        "html_url": issue["html_url"],
+        "reactions_laugh": issue["reactions"]["laugh"],
+        "reactions_hooray": issue["reactions"]["hooray"],
+        "reactions_confused": issue["reactions"]["confused"],
+        "reactions_heart": issue["reactions"]["heart"],
+        "reactions_rocket": issue["reactions"]["rocket"],
+        "reactions_eyes": issue["reactions"]["eyes"],
+    }
+    issues_reduced.append(issue_reduced)
+
+with open("twitter-issues.json", "w") as f:
+    json.dump(issues_reduced, f)
+
+print(f"Retrieved {len(all_issues)} issues and saved to twitter-issues.json")
